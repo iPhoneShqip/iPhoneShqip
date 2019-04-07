7c6ca28321bdf74382149e4acffd70610f28b1ea
iPhoneShqip <iPhoneShqip@hotmail.com>
2019-04-07 19:51:08 -0400

    Update

---
+++ pages/blackdockv2/changelog.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8"><title>Changelog</title><base target="_top">
+<link rel="stylesheet" type="text/css" href="http://ishqip.xyz/images/style.css">
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"><meta name="format-detection" content="telephone=no">
+
+<script type="text/javascript" src="http://ishqip.xyz/images/thankssaurik.js"></script>
+
+</head>
+
+<body class="pinstripe">
+
+<panel>
+
+<block >
+<p>Changelog - Black Dock v2</p>
+</block>
+<br>
+<p align="center"><strong> v 1.0</strong></p>
+
+<fieldset>
+<div><center>
+                
+<p> Initial release. </p> </center> 
+</div>
+
+</fieldset>
+
+
+</panel>
+
+</body>
+</html>
