f1954833702cbb8944a07d3e141a5df796650e37
iPhoneShqip <iPhoneShqip@hotmail.com>
2019-04-08 11:58:50 -0400

    Update

---
+++ pages/adblockr/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html class=" cytyle-flat"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+<meta charset="utf-8"><title>AdBlock R+</title><!-- base href="http://ishqip.xyz/" -->
+<link rel="stylesheet" type="text/css" href="http://ishqip.xyz/images/style.css">
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"><meta name="format-detection" content="telephone=no">
+
+<script type="text/javascript" src="http://ishqip.xyz/images/thankssaurik.js"></script><!-- base href="http://ishqio.xyz/" --><style type="text/css"> p, input[type="password"], input[type="text"], select { top: 0.022058823529411766em; } </style>
+
+</head>
+
+<body class="pinstripe">
+
+<panel>
+
+<fieldset>
+<a class="" href="screenshots.php"><img src="http://ishqip.xyz/images/screenshot.png" alt="" class="icon" height="58" width="58"><div><div><label><p>Screenshots</p></label></div></div></a>
+<center>
+  <br>
+</center>
+
+<div>
+  <center>
+    <p>
+      <label style="color:#595959; font-size:25px;"><strong>AdBlock R+</strong></label>
+    </p>
+    <br/>
+    <center>
+    </center>
+    <div itemprop="description">
+      <p>AdBlock allows you to block ads on your iOS device.</p>
+      <p>The original AdBlock for iOS 8 and iOS 9 (32-bit and 64-bit). Blocking ads on iPhones and iPads since 2012.<br>
+        <br>
+        AdBlock allows you to block ads on your iOS device. It works with every app, not just Safari.</p>
+      <p>Get rid of 99% of annoying popups, banners and video ads in all of your apps, including games and entertainment apps.</p>
+      <p>Protect your privacy, limit bandwidth use, speed up your device and improve up your overall iOS experience.<br>
+        <br>
+        <strong>The most important advantages of AdBlock for iOS are:</strong></p>
+      <p>• AdBlock works with Wi-Fi and cellular networks. You don't need to set it up separately for every network you use.</p>
+      <p>• AdBlock works on any iOS 8 and iOS 9 device, even if it's not   64-bit. Take advantage of ad blocking even on older iPhone and iPad.</p>
+      <p>• AdBlock works seamlessly with other iOS apps displaying ads served   over the Internet. Forget about ads in games, utilities and on websites.</p>
+      <p>• AdBlock allows you to create your own list of blocked domains and   synchronise your filters between all your devices using iCloud.</p>
+      <p>• AdBlock allows you to protect your privacy by blocking mobile   trackers. Prevent Internet trackers from recording of your web and   in-app activity.</p>
+      <p>• AdBlock does not send your Internet traffic through proxy or VPN server. All of your connections stay direct, fast and secure.</p>
+      <p>• Setting up AdBlock for iOS is very simple! Just download the app,   enable the "magic switch" and tap the "Install" button. It takes less   than 20 seconds to complete.</p>
+      <p>• AdBlock automatically updates to the latest version of the filters   on every app run. We keep the ad servers database up-to-date.</p>
+      <p>• AdBlock works with Safari and 99% of other apps (including other   browsers like Chrome). Benefit from ad blocking in your favourite   Internet browser on iOS.</p>
+      <p><br>
+        <em>Limitations:</em></p>
+      <p>• AdBlock is not compatible with VPN connections. You can't use AdBlock simultaneously with VPN.</p>
+      <p>• Blocking ads may not work on networks where DNS traffic is intercepted by the ISP</p>
+    </div>
+    <p>&nbsp;</p>
+    <p><br/>
+    <p> -----------------------------<br/>
+      SUPPORTS: iPhone, iPad &amp; iPod Touch on iOS 7.0 to iOS 9.1
+    </center>
+</div>
+<a href="changelog.php"><img src="http://ishqip.xyz/images/changelog.png" alt="" class="icon" height="58" width="58"><div><div><label><p>Full Changelog</p></label></div></div></a>
+</fieldset>
+
+<fieldset>
+<a class="" href="https://twitter.com/iPhShqip"><img src="http://ishqip.xyz/images/twitter.png" alt="" class="icon" height="58" width="58"><div><div><label><p>Follow @iPhoneShqip</p></label></div></div></a>
+
+<a class="" href="http://iphoneshqip.tumblr.com/"><img src="http://ishqip.xyz/images/tumblr.png" alt="" class="icon" height="58" width="58"><div><div><label><p>Follow on Tumblr</p></label></div></div></a>
+</fieldset>
+<fieldset>
+  <a class="" href="mailto:iPhoneShqip@hotmail.com" target="_blank"><img src="http://ishqip.xyz/images/mail.png" alt="" class="icon" height="58" width="58">
+  <div><div><label><p>E-Mail Me</p></label></div></div></a>
+</fieldset>
---
+++ pages/cylindereffects/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html class=" cytyle-flat"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+<meta charset="utf-8"><title>Cylinder Effects</title><!-- base href="http://ishqip.xyz/" -->
+<link rel="stylesheet" type="text/css" href="http://ishqip.xyz/images/style.css">
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"><meta name="format-detection" content="telephone=no">
+
+<script type="text/javascript" src="http://ishqip.xyz/images/thankssaurik.js"></script><!-- base href="http://ishqio.xyz/" --><style type="text/css"> p, input[type="password"], input[type="text"], select { top: 0.022058823529411766em; } </style>
+
+</head>
+
+<body class="pinstripe">
+
+<panel>
+
+<fieldset>
+<a class="" href="screenshots.php"><img src="http://ishqip.xyz/images/screenshot.png" alt="" class="icon" height="58" width="58"><div><div><label><p>Screenshots</p></label></div></div></a>
+<div>
+  <center>
+    <p>
+      <label style="color:#595959; font-size:25px;"><strong>Cylinder Effects</strong></label>
+    </p>
+    <br/>
+    <center>
+    </center>
+    <p>Effects Cylinder for iPhone, iPad and iPod Touch. </p>
+    <p> -------------------------<br/>
+    </p>
+  </center>
+  <p>SUPPORTS: iPhone 3GS/4/5/5S/6/6+/6S/6S+ iPod Touch and iPad on iOS 4.x to 9.0.2</p>
+  <p>Main tweaks required Cylinder. </p>
+  <p>Apply via Cylinder on Effects in Settings.</p>
+  <p>&nbsp;</p>
+</div>
+<a href="changelog.php"><img src="http://ishqip.xyz/images/changelog.png" alt="" class="icon" height="58" width="58"><div><div><label><p>Full Changelog</p></label></div></div></a>
+</fieldset>
+<fieldset>
+  <a href="https://twitter.com/CydiaShqip" target="_blank"><img src="http://ishqip.xyz/images/twitterdev.png" alt="" class="icon" height="58" width="58"><div><div><label><p>Follow Developer</p></label></div></div></a>
+
+<a class="" href="https://twitter.com/CydiaShqip"><img src="http://ishqip.xyz/images/twitter.png" alt="" class="icon" height="58" width="58"><div><div><label><p>Follow @iSHQ!P</p></label></div></div></a>
+
+<a class="" href="http://iphoneshqip.tumblr.com/"><img src="http://ishqip.xyz/images/tumblr.png" alt="" class="icon" height="58" width="58"><div><div><label><p>Follow on Tumblr</p></label></div></div></a>
+</fieldset>
+<fieldset>
+  <a class="" href="mailto:iPhoneShqip@hotmail.com" target="_blank"><img src="http://ishqip.xyz/images/mail.png" alt="" class="icon" height="58" width="58">
+  <div><div><label><p>E-Mail Me</p></label></div></div></a>
+
+</fieldset>
+<fieldset>
+
+  <!--
+<a href="https://twitter.com/Nyk0n_" target="_blank"><img src="../depictions/images/twitter.png" alt="" width="58" height="58" class="icon">
+<div><div><label><p>@Nyk0n</p></label></div></div></a>-->
+
+</fieldset>
+
+<!--
+<block>
+<img class="img-valign" <img src="../depictions/images/download.png" width="28" height="28" alt="" />
+<span class="text1">downloads:</span>
+</block>
+-->
+
+<!-- Downloads Code -->
+						
+			<script language="javascript">
+				function showstats() {
+					var stats = document.getElementById("stats");
+					if(stats.style.display == "none") {
+						stats.style.display = "block";
+					}
+					else {
+						stats.style.display = "none";
+					}
+				}
+			</script>
+			<fieldset>
+		</fieldset>
+ 
+
+</panel>
+
+
+<!--Javascript-->
+<script type="text/javascript">function iOSversion() {
+	if (/iP(hone|od|ad)/.test(navigator.platform)) {
+		var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+		return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+	} else {
+		return 0;
+	}
+}
+ver = iOSversion();
+var versionNumber = parseFloat(ver[0]+'.'+ver[1]);
+if (versionNumber >= 7 && versionNumber <= 9.1) {
+	document.getElementById('version').innerHTML='<p>😊 Your device is supported.</p>';
+} else {
+	document.getElementById('version').innerHTML='<p>😔 Your device is not supported.</p>';
+}</script>
+<!--Javascript end-->
+
+
+</body></html>
